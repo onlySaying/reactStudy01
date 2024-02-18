29de04a2539d71f2509fe9bd983be7e39921a7f2
onlySaying <48788534+onlySaying@users.noreply.github.com>
2024-02-18 22:10:34 +0900

    dd

--- reactStrudy/week01/day2/day2practice/src/index.js
+++
@@ -1,9 +0,0 @@
-import react from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-const element = document.getElementById("root");
-const root = ReactDOM.createRoot(element);
-
-
-root.render(<App/>)
