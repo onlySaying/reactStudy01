883d8767dc6352f1ea671afa46e4d2aa0daceaea
unknown <c960612@gmail.com>
2024-02-21 15:06:55 +0900

    study props

--- reactStrudy/lesson01/01-starting-project/src/App.jsx
+++ reactStrudy/lesson01/01-starting-project/src/App.jsx
@@ -1,12 +1,24 @@
-function App() {
+import reactImg from './assets/react-core-concepts.png'
+import componentsImg from './assets/Components.png'
+
+const reactDescriptions = ['Fundamental', 'cucial', 'Core'];
+
+function genRandomInt(max)
+{
+  return Math.floor(Math.random() * (max));
+}
+
+function Header()
+{
+  const description = reactDescriptions[genRandomInt(reactDescriptions.length)] 
   return (
     <div>
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
+          {description}
+          React concepts you will need for almost any app you are
         </p>
       </header>
       <main>
@@ -14,6 +26,39 @@ function App() {
       </main>
     </div>
   );
+}
+function CoreConcept(props)
+{
+  return (
+    <li>
+      <img src = {props.image} alt = {props.title}/>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
+function App() {
+  return(
+    <div>
+      <Header/>
+      <main>
+        <section id = "core-concepts">
+        <h2>Core Concept</h2>
+        <ul>
+          <CoreConcept title = "Components" 
+          description ="The core ui building block."
+          image = {componentsImg}/>
+          <CoreConcept title = "Props" description ="" />
+          <CoreConcept title = "Components" description ="" />
+          <CoreConcept title = "Components" description ="" />
+
+        </ul>
+        </section>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+   
+  )
 }
 
 export default App;
