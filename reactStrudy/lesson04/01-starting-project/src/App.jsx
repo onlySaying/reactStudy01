6e1c9458dd576830cec5e8cd391dfc0226b4956a
onlySaying <48788534+onlySaying@users.noreply.github.com>
2024-03-12 09:24:17 +0900

    studyReact

--- reactStrudy/lesson04/01-starting-project/src/App.jsx
+++ reactStrudy/lesson04/01-starting-project/src/App.jsx
@@ -1,6 +1,33 @@
+import Header from "./components/Header"
+import Results from "./components/Results";
+import UserInput from "./components/UserInput";
+import { useState } from "react"
+
 function App() {
+  const[userInput, setUserInput] = useState
+({
+      initialInvestment : 10000,
+      annualInvestment : 12000,
+      expectedReturn : 6,
+      duration : 10 
+});
+
+const inputIsValid =userInput.duration>=1;
+
+function handleChange(inputIdentifier, newValue)
+{ setUserInput(prevUserInput =>
+      { return { ...prevUserInput,
+          [inputIdentifier] : +newValue }
+      })
+}
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <Header/>
+      <UserInput userInput = {userInput} onChange={handleChange}/>
+      {inputIsValid &&< Results userInput = {userInput} />}
+      {!inputIsValid && <p className="center">please enter
+      a duration greater than zero</p>}
+    </>
   )
 }
 
